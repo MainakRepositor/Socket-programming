32b2c8eca241f48f392e449af55f6371d3e1cb3e
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-08-31 14:53:07 +0530

    Update client.c

--- C/Full Duplex TCP/client.c
+++ C/Full Duplex TCP/client.c
@@ -1,15 +1,15 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+<include stdio.h>
+<include stdlib.h>
+<include string.h>
 //headers for socket and related functions
-#include <sys/types.h>
-#include <sys/socket.h>
+<include <sys/types.h>
+<include <sys/socket.h>
 //for including structures which will store information needed
-#include <netinet/in.h>
-#include <unistd.h>
+<include <netinet/in.h>
+<include <unistd.h>
 //for gethostbyname
-#include "netdb.h"
-#include "arpa/inet.h"
+<include netdb.h>
+<include arpa/inet.h>
 
 int main()
 {
