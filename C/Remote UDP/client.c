95f0a048244b038f2cdc3491cfedda3076bc142d
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-09-28 13:57:50 +0530

    Create client.c

---
+++ C/Remote UDP/client.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define MAX 1000
+
+int main()
+{
+    int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    char buffer[MAX], message[MAX];
+    struct sockaddr_in cliaddr, serverAddress;
+    socklen_t serverLength = sizeof(serverAddress);
+
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(9976);
+
+    bind(serverDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    while (1)
+    {
+        printf("\nENTER A COMMAND FOR EXECUTION ... ");
+        fgets(buffer, sizeof(buffer), stdin);
+        sendto(serverDescriptor, buffer, sizeof(buffer), 0, (struct sockaddr *)&serverAddress, serverLength);
+        printf("\nData Sent !");
+        recvfrom(serverDescriptor, message, sizeof(message), 0, (struct sockaddr *)&serverAddress, &serverLength);
+        printf("UDP SERVER : %s", message);
+    }
+    return 0;
+}
