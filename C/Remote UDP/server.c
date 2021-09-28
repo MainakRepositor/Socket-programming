7cde6c773924ebb69559c08cd368f92b1ad7787b
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-09-28 13:58:47 +0530

    Create server.c

---
+++ C/Remote UDP/server.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define MAX 1000
+int main()
+{
+
+    int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    int size;
+    char buffer[MAX], message[] = "Command Successfully executed !";
+    struct sockaddr_in clientAddress, serverAddress;
+
+    socklen_t clientLength = sizeof(clientAddress);
+
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(9976);
+
+    bind(serverDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    while (1)
+    {
+        bzero(buffer, sizeof(buffer));
+        recvfrom(serverDescriptor, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddress, &clientLength);
+        system(buffer);
+        printf("Command Executed ... %s ", buffer);
+        sendto(serverDescriptor, message, sizeof(message), 0, (struct sockaddr *)&clientAddress, clientLength);
+    }
+    close(serverDescriptor);
+    return 0;
+}
